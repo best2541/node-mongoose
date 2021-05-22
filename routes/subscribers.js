89c497e4f9a54fe7a9f7ee90bf857b3b3b85bee8
BASS <PAWAT_BEST@WINDOWSLIVE.COM>
2021-05-22 18:04:43 +0700

    add successful

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
     res.send(req.params.id)
 })
-
+//add
 router.post('/', async (req, res) => {
     const subscriber = new Subscriber({
         name: req.body.name,
